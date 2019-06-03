44b2d27c3fd8148e232d4ec6fd21df796db461c2
shelly <shellynacole@yahoo.com>
2019-06-03 19:18:56 -0400

    First Commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var tempFahr  = "Since it is less than 54 degrees ";
+var eventType = "and you are going to a formal event, ";
+var clothing = "you should wear a suit and a coat.";
+var res = tempFahr.concat(eventType, clothing);
+console.log(res);
+
+var tempFahr = "Since it is between 54-70 degrees ";
+var eventType = "and you are going to a semi-formal event, ";
+var clothing = "you should wear a polo and a jacket.";
+var res = tempFahr.concat(eventType, clothing);
+console.log(res);
+
+var tempFahr = "Since it is more than 70 degrees ";
+var eventType = "and you are going to casual event, ";
+var clothing = "you should wear something comfy and no jacket.";
+var res = tempFahr.concat(eventType, clothing);
+console.log(res);
+
+
